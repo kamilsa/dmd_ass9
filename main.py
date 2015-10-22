f24abbdb4667cfa4ff964164a811656807bc2fc8
kamilsa <kamilsa16@gmail.com>
2015-10-22 13:36:14 +0300

    DB with buggy working inserts

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from Profiler import Profiler
 from page import page
 from hash.ex_hash import EH
 from student import student
 import names
 import faker
 import pickle
+import time
 
 __author__ = 'kamil'
 
@@ -41,18 +43,40 @@ def page_test():
 
 def db_test():
     filename = 'student.txt'
-    # mydb = EH(filename)
-    # studs = []
-    # for i in range(1, 100):
-    #     studs.append(get_random_student(i))
-    #
-    # for stud in studs:
-    #     mydb.put(stud._id, stud)
-    #
-    # save(mydb)
-    mydb = load()
-    print(mydb.get(5))
+    mydb = EH(filename)
+    studs = []
+    for i in range(1, 10000):
+        if i % 100 == 0:
+            print('#',i)
+        studs.append(get_random_student(i))
+    print('inserting...')
+    with Profiler() as p:
+        for stud in studs:
+            mydb.put(stud._id, stud)
+            print('#',stud._id,'added')
 
+    save(mydb)
+    # mydb = load()
+    print(mydb.get(6))
 
-db_test()
+def generate_dataset():
+    filename = 'to_put.txt'
+    f = open(filename, 'w')
+    for i in range(1, 10000):
+        if i % 100 == 0:
+            print('#',i)
+
+        student = get_random_student(i)
+        to_ins = str(student._id) + ' ' + student._name + ' ' + student._address + ' ' + student._email + '\n'
+        f.write(to_ins)
+    f.close()
+
+def get_dataset():
+    filename = 'to_put.txt'
+    f = open(filename, 'r')
+    line = f.readline()
+    toks = line.split(' ')
+
+generate_dataset()
+# db_test()
 # page_test()
